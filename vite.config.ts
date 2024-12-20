d3379e32f8168ba7c5e308ba0a02c3fd3e0173da
Fr3nk <bugugnolifrancesco@gmail.com>
2024-12-20 15:43:08 +1100

    Initial setup

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,52 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import { viteStaticCopy } from "vite-plugin-static-copy";
+import path from "path";
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
-})
+  plugins: [
+    react(),
+    viteStaticCopy({
+      targets: [
+        {
+          src: "manifest.json",
+          dest: ".",
+        },
+        {
+          src: "public/*",
+          dest: ".",
+        },
+      ],
+    }),
+  ],
+  resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "./src"),
+    },
+  },
+  build: {
+    outDir: "dist",
+    rollupOptions: {
+      input: {
+        main: "index.html",
+      },
+      output: {
+        entryFileNames: `assets/[name].js`,
+        chunkFileNames: `assets/[name].js`,
+        assetFileNames: `assets/[name].[ext]`,
+      },
+    },
+    sourcemap: "inline",
+    modulePreload: false,
+    target: "esnext",
+    minify: false,
+  },
+  server: {
+    port: 5173,
+    strictPort: true,
+    hmr: {
+      port: 5173,
+    },
+  },
+});
